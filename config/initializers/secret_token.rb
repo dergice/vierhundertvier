4a083a44fdfe369c67d29da3906684797bd7471b
Andreas Geissinger <andi@fream.com>
2012-10-31 12:17:23 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vierhundertvier::Application.config.secret_token = '3dfb6e3ca381531c582c8c2fa9d5fe275413e9dc8f45a053379464267fb217c1bd51fe59125ddc4623831c458778bc6f567577be6a2d2d573c21578658176421'
